2ddceb454643a8ae87a54176f0a5eb44d6c1915d
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-06-11 00:32:36 +0200

    delete operation added;

--- Desktop/Course_Reset/genius-car-services-server/index.js
+++ Desktop/Course_Reset/genius-car-services-server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -13,20 +13,53 @@ app.use(cors());
 app.use(express.json());
 
 
-
 // Replace the uri string with your MongoDB deployment's connection string.
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lrr7vme.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lrr7vme.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 async function run () {
     try {
-        const collection = client.db("test").collection("devices");
-        console.log("Genius car db connected");
+        await client.connect();
+        const serviceCollection = client.db("geniusCar").collection("service");
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get("/service/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };//for find one
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        //post data to server
+        app.post('/service', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        });
+
+        //DELETE
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+
+        })
+
     } finally {
-        await client.close();
+        // Ensures that the client will close when you finish/error
+        // await client.close();
     }
 }
 run().catch(console.dir);
-
 
 app.get('/', (req, res) => {
     res.send('Running genius server ');
